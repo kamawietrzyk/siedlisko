cb32f091e072f79275710b2ff3a58c33c9aa95da
kamyk238 <kama.violinist@gmail.com>
2020-01-06 23:38:17 +0100

    basics+logo

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from './logo.svg'
+import './index.scss'
+
+const Header = () => (
+    <div className="header">
+      <img src={logo} className="logo" alt="logo" height="200" />
+    </div>
+)
+
+export default Header
