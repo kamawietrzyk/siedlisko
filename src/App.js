9003a3c0321effc5f1777cacd738ffcb991ade63
kamyk238 <kama.violinist@gmail.com>
2020-01-08 12:05:27 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route, Switch } from 'react-router'
+import URLS from './config/urls'
+import HomeView from './views/Home'
+import AboutUsView from './views/AboutUs'
 import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-        <Header />
+    <div className="container">
+    <Header />
+    <Switch>
+      <Route exact path={URLS.HOME} component={HomeView} />
+      <Route exact path={URLS.ABOUT_US} component={AboutUsView} />
+    </Switch>
     </div>
   );
 }
---
+++ src/views/AboutUs.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AboutUsView = () => (
+    <>
+        <h1>O nas</h1>
+        <p>Lipsum</p>
+    </>
+)
+
+export default AboutUsView
