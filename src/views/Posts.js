c2270d1937188697248c9a511de71e0ed4f2c62a
kamyk238 <kama.violinist@gmail.com>
2020-01-22 10:18:16 +0100

    ScrollToTop added to components

--- src/views/Posts.js
+++ src/views/Posts.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Posts from '../components/Posts'
+import ScrollToTopOnMount from '../components/ScrollToTop'
 
 const PostsView = () => (
   <div className="container p-0">
+    <ScrollToTopOnMount />
     <Posts />
   </div>
 )
