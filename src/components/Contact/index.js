5aa9ee3846858eac5373d8fd9577b3d26495c0e4
kamyk238 <kama.violinist@gmail.com>
2020-01-29 15:46:08 +0100

    Permaculture page + style + some fixes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,22 +6,22 @@ import { faHome, faPhoneSquareAlt, faAt } from '@fortawesome/free-solid-svg-icon
 
 const Contact = () => (
   <>
-    <div>
-      <img src={kontakt} alt="contact pic" width="100%" />
-    </div>
-    <h1 className="text-center pt-4 mt-5 mb-0 bg-white">Odezwij się do nas!</h1>
+    <img src={kontakt} alt="contact pic" width="100%" />
     <div className="Contact">
-      <div>
-        <h2><FontAwesomeIcon icon={faHome} /> Adres</h2>
-        <p>Siedlisko Góra Wiatrów <br/> 32-855 Gwoździec</p>
-      </div>
-      <div>
-        <h2><FontAwesomeIcon icon={faPhoneSquareAlt} /> Telefon</h2>
-        <p>Tomek Wietrzyk <br/> (+48) 690-005-927</p>
-      </div>
-      <div>
-        <h2><FontAwesomeIcon icon={faAt} /> Email</h2>
-        <p>Kama Swoboda-Wietrzyk <br/> kama@wietrzyk.com</p>
+      <h1 className="text-center pt-4 mt-5 mb-0">Odezwij się do nas!</h1>
+      <div className="Contact__details">
+        <div>
+          <h2><FontAwesomeIcon icon={faHome} /> Adres</h2>
+          <p>Siedlisko Góra Wiatrów <br/> 32-855 Gwoździec</p>
+        </div>
+        <div>
+          <h2><FontAwesomeIcon icon={faPhoneSquareAlt} /> Telefon</h2>
+          <p>Tomek Wietrzyk <br/> (+48) 690-005-927</p>
+        </div>
+        <div>
+          <h2><FontAwesomeIcon icon={faAt} /> Email</h2>
+          <p>Kama Swoboda-Wietrzyk <br/> kama@wietrzyk.com</p>
+        </div>
       </div>
     </div>
   </>
