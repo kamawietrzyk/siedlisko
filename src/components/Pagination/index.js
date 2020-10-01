c4c4327421fe0751b321a4a6bc6d98a6c8b8c1db
Kama <kama@wietrzyk.com>
2020-10-01 23:37:15 +0200

    New post + style updates

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -8,10 +8,10 @@ const Pagination = ({ onChange, currentPage, totalPages }) => (
     <nav aria-label="navigation">
       <ul className="pagination justify-content-center">
         <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
-          <a onClick={onChange('prev')} className="page-link" href="#"><FontAwesomeIcon icon={faAngleDoubleLeft} className="pr-1"/></a>
+          <a onClick={onChange('prev')} className="page-link" href=" "><FontAwesomeIcon icon={faAngleDoubleLeft} className="pr-1"/></a>
         </li>
         <li className={`page-item ml-1 ${currentPage === totalPages && 'disabled'}`}>
-          <a onClick={onChange('next')} className="page-link" href="#"><FontAwesomeIcon icon={faAngleDoubleRight} className="pr-1"/> </a>
+          <a onClick={onChange('next')} className="page-link" href=" "><FontAwesomeIcon icon={faAngleDoubleRight} className="pr-1"/> </a>
         </li>
       </ul>
     </nav>
